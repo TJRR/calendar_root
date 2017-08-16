34372cfe096d76c8e3290885ca3e6e00a6652f1f
Alan Velasques Santos <alanvelasques@gmail.com>
2017-08-16 17:32:43 +0000

    Retirado o curso de hardcoded

--- lib.php
+++ lib.php
@@ -425,7 +425,7 @@ function calendar_get_mini($courses, $groups, $users, $calmonth = false, $calyea
                 }
                 if($event->modulename == 'bigbluebuttonbn'){
                    $eventhref = new moodle_url('../mod/'.$event->modulename.'/view.php');
-                   $id_evendo_bbb = $DB->get_record_sql('SELECT cm.id FROM {course_modules} cm inner join {modules} m on cm.module = m.id inner join {event} me on cm.instance = me.instance where me.modulename="bigbluebuttonbn" AND me.instance = ? AND cm.course=2 AND m.name="bigbluebuttonbn"', array($event->instance));
+                   $id_evendo_bbb = $DB->get_record_sql('SELECT cm.id FROM {course_modules} cm inner join {modules} m on cm.module = m.id inner join {event} me on cm.instance = me.instance where me.modulename="bigbluebuttonbn" AND me.instance = ? AND cm.course= ? AND m.name="bigbluebuttonbn"', array($event->instance, $courseid));
                    $eventhref .= '?id='.$id_evendo_bbb->id;
                    $popupcontent .= html_writer::link($eventhref, $name);
                 }else{
